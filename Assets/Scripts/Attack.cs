f465181aa134f67133ac1d213334bf96961101a5
alybickham <bickhaml@email.sc.edu>
2023-11-30 03:17:43 -0500

    fixed error ;

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
 
-    GameObject playerWeapon
+    GameObject playerWeapon;
 
     // Start is called before the first frame update
     void Start()
