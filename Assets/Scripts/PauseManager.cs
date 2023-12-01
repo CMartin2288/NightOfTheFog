82004b67f653b75d9ba5ad11aeb3beb4e20c7d6e
Caleb Martin <cam228@outlook.com>
2023-12-01 17:01:05 -0500

    Pause Menu

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool paused;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        paused = false;
+        pauseMenu = GameObject.FindWithTag("PauseMenu");
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel"))
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        if(!paused)
+        {
+            paused = true;
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
+            pauseMenu.SetActive(true);
+        }
+        else
+        {
+            paused = false;
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+            pauseMenu.SetActive(false);
+        }
+    }
+}
