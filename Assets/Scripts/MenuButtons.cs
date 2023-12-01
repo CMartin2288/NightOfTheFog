f2a6f812c284d48fecae30e35f6072a8d53b2e9d
Caleb Martin <cam228@outlook.com>
2023-11-30 19:22:24 -0500

    Prefab fixes, Menu Buttons, Credits Scene

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ChangeScene(string sceneName)
     {
-        
+        SceneManager.LoadSceneAsync(sceneName);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Quit()
     {
-        
+        Application.Quit();
     }
 }
