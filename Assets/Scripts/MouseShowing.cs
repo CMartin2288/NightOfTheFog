ce1dd14d7cbb9f8b4fba958970c0d17c0b0a1f1a
alybickham <bickhaml@email.sc.edu>
2023-12-13 17:47:58 -0500

    added timer to win and lose screens

--- Assets/Scripts/MouseShowing.cs
+++ Assets/Scripts/MouseShowing.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class MouseShowing : MonoBehaviour
+
 {
+    private TimeSpan displayTimer;
+    public TMP_Text timerText;
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+        displayTimer = TimeSpan.FromSeconds(SaveManager.timer);
+        timerText.text += displayTimer.ToString(@"mm\:ss");
     }
 
     // Update is called once per frame
